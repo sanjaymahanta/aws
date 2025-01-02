137b7b98537dadb45244353bcfcac907111bac29
sanjaymahanta <168723809+sanjaymahanta@users.noreply.github.com>
2025-01-02 22:29:14 +0530

    First Commit

---
+++ src/test/java/com/aws/FirstTest.java
@@ -0,0 +1,25 @@
+package com.aws;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	
+	
+	@Test
+	public void openBrowser() {
+		
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "D:/chromedriver131.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		driver = new ChromeDriver(options);
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		
+	}
+}
